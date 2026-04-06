00d7c42ac62a8250d6ec02400a15db295639c0ea
Daniel Anderson Campos <dev.danielanderson@gmail.com.br>
2026-04-06 11:59:20 -0400

    finalizando controller cart

--- src/main/java/com/example/ecomerce/controller/CartController.java
+++ src/main/java/com/example/ecomerce/controller/CartController.java
@@ -1,4 +1,62 @@
 package com.example.ecomerce.controller;
 
+import com.example.ecomerce.dto.response.CartResponse;
+import com.example.ecomerce.entity.CartEntity;
+import com.example.ecomerce.mapper.CartMapper;
+import com.example.ecomerce.service.CartService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/cart")
 public class CartController {
+
+    private  final CartService cartService;
+    private  final CartMapper cartMapper;
+
+    public  CartController( CartService cartService , CartMapper cartMapper){
+        this.cartService = cartService;
+        this.cartMapper = cartMapper;
+
+    }
+
+    @PostMapping("/{cartId}items")
+    public ResponseEntity<Void> addProduct(
+            @PathVariable UUID cartId,
+            @RequestParam UUID productId,
+            @RequestParam int quantity
+            ){
+
+        cartService.addProduct(cartId,productId,quantity);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{cartId}/items")
+    public  ResponseEntity<Void>  updateProduct(
+            @PathVariable UUID cartId,
+            @RequestParam UUID productId,
+            @RequestParam int quantity
+    ) throws IllegalAccessException {
+        cartService.updateProduct(cartId,productId,quantity);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{cartId}/items/{productId}")
+    public ResponseEntity<Void> removeProduct(
+            @PathVariable UUID cartId,
+            @PathVariable UUID productId
+    ){
+        cartService.removeProduct(cartId,productId);
+        return  ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{cartId}")
+    public ResponseEntity<CartResponse> getCart(
+            @PathVariable UUID cartId
+    ){
+        CartEntity cart = cartService.getCartById(cartId);
+        return ResponseEntity.ok(cartMapper.toResponse(cart));
+    }
 }
