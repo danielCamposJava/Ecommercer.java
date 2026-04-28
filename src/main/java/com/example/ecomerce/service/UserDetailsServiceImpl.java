19671be2b3ec5cb748107b1b1d2b1ac52bbe8f25
Daniel Anderson Campos <dev.danielanderson@gmail.com.br>
2026-04-28 13:15:29 -0400

    Craindo tela de produto

--- src/main/java/com/example/ecomerce/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/ecomerce/service/UserDetailsServiceImpl.java
@@ -3,7 +3,6 @@ package com.example.ecomerce.service;
 import com.example.ecomerce.entity.UserEntity;
 import com.example.ecomerce.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,16 +17,14 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        //  Busca o usuário no banco usando o método que você criou
+
         UserEntity user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + email));
 
-        //  Converte sua UserEntity para o UserDetails do Spring Security
-        // Nota: Se sua UserEntity já implementar UserDetails, você pode retornar 'user' direto.
         return org.springframework.security.core.userdetails.User.builder()
                 .username(user.getEmail())
-                .password(user.getPassword()) // Deve estar em BCrypt
-                .authorities(user.getRole().name()) // Assume que você tem um Enum Role
+                .password(user.getPassword())
+                .authorities(user.getRole().name())
                 .build();
     }
 }
